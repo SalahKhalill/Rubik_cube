c8235fddfb85a61da29c265fef975cc2145d6483
Salah Khalil <salahhamed111@gmail.com>
2024-12-11 01:38:58 +0300

    updates on history and home

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,24 +6,17 @@ const Footer = () => {
     <div className="footer-wrapper btm u-bg-black">
       <div className="container w-container">
         <div className="footer__legal-content">
-          <a
-            id="w-node-e9e16bf4-9e79-c7f3-fd3b-fb5372b5a63d-72b5a620"
-            href="https://www.spinmaster.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="footer__sm-logo w-inline-block"
-          >
+        
             <img
               src={logo}
               loading="lazy"
               width="100"
               alt="Spin Master Logo"
             />
-          </a>
           <div className="footer__legal-wrapper">
             <div className="footer__legal-links">
               <a
-                href="https://www.spinmaster.com/en-US/contact-us/"
+                onClick={() => window.location.href = '/contact'}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="legal-link"
@@ -31,7 +24,6 @@ const Footer = () => {
                 Contact Us
               </a>
               <a
-                href="https://privacy.spinmaster.com/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="legal-link"
